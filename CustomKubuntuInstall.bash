406f5284c118ebc697755de67c437d022486ea42
jointri <81046576+jointri@users.noreply.github.com>
2022-07-24 11:52:13 -0400

    Update CustomKubuntuInstall.bash

--- CustomKubuntuInstall.bash
+++ CustomKubuntuInstall.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#This script is to be ran AFTER you setup the partitions and debootstrap 'debootstrap --include=lsb_release,gnupg,wget,git,curl,vim --arch=amd64 jammy <chroot root>'
+#This script is to be ran AFTER you setup the partitions and debootstrap 'debootstrap --include=lsb-release,gnupg,wget,git,curl,vim --arch=amd64 jammy <chroot root>'
 #Bad things will happen if you miss a dependency or didn't setup the partitions properly.
 #It's also import to have done genfstab -U <mountpoint> >> <mountpoint>/etc/fstab along with arch-chroot <mountpoint> because the script is to be ran inside the chroot
 
